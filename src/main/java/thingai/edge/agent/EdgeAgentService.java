0b765977a6a7cc4bb0637ac8154f2aafb559581f
lgthvinh <everwellmax@gmail.com>
2026-04-16 23:01:28 +0700

    Add SLF4J logging support to EdgeAgentService

--- src/main/java/thingai/edge/agent/EdgeAgentService.java
+++ src/main/java/thingai/edge/agent/EdgeAgentService.java
@@ -1,14 +1,26 @@
 package thingai.edge.agent;
 
 import org.thingai.base.Service;
+import org.thingai.base.log.ILog;
 
 public class EdgeAgentService extends Service {
-    public String name = "EdgeAgentService";
-    public String version = "0.1.0";
-    public String appDirName = "edge-agent";
+    private static final String TAG = "EdgeAgentService";
+
+    protected EdgeAgentService() {
+        super("edge-agent");
+        setVersion("0.1.0");
+        setAppDirName("edge-agent");
+        ILog.ENABLE_LOGGING = true;
+        ILog.logLevel = ILog.DEBUG;
+    }
 
     @Override
     protected void onServiceInit() {
+        ILog.d(TAG, "onServiceInit");
+    }
+
+    @Override
+    protected void onServiceShutdown() {
 
     }
 }
